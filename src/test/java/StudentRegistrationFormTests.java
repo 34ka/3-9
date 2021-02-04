57901629fd973b7d9898ba4c76886fc732e40134
34ka <60933524+34ka@users.noreply.github.com>
2021-02-05 03:31:00 +0600

    added allure steps

--- src/test/java/StudentRegistrationFormTests.java
+++ src/test/java/StudentRegistrationFormTests.java
@@ -11,6 +11,7 @@ import java.util.Locale;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
+import static io.qameta.allure.Allure.step;
 
 public class StudentRegistrationFormTests {
 
@@ -46,56 +47,60 @@ public class StudentRegistrationFormTests {
                 picture = "1.jpg",
                 state = "Rajasthan",
                 city = "Jaiselmer";
+        step("Open students registration form", () -> {
+            open("https://demoqa.com/automation-practice-form");
+            $(".practice-form-wrapper").shouldHave(text("Student Registration Form"));
+        });
 
-        open("https://demoqa.com/automation-practice-form");
-        $(".practice-form-wrapper").shouldHave(text("Student Registration Form"));
+        step("Fill students registration form", () -> {
+            $("#firstName").val(firstName);
+            $("#lastName").val(lastName);
+            $("#userEmail").val(userEmail);
+            $("#genterWrapper").$(byText(gender)).click();
+            $("#userNumber").val(userNumber);
+            //set date
+            $("#dateOfBirthInput").click();
+            $(".react-datepicker__month-select").selectOption(monthOfBirth);
+            $(".react-datepicker__year-select").selectOption(yearOfBirth);
+            $(".react-datepicker__day--0" + dayOfBirth).click();
+            //set subjects
+            $("#subjectsInput").val(subject1Prefix);
+            $(".subjects-auto-complete__menu-list").$(byText(subject1)).click();
+            $("#subjectsInput").val(subject2Prefix);
+            $(".subjects-auto-complete__menu-list").$(byText(subject2)).click();
+            $("#subjectsInput").val(subject3Prefix);
+            $(".subjects-auto-complete__menu-list").$(byText(subject3)).click();
+            //set hobbies
+            $("#hobbiesWrapper").$(byText(hobbie1)).click();
+            $("#hobbiesWrapper").$(byText(hobbie2)).click();
 
-        $("#firstName").val(firstName);
-        $("#lastName").val(lastName);
-        $("#userEmail").val(userEmail);
-        $("#genterWrapper").$(byText(gender)).click();
-        $("#userNumber").val(userNumber);
-        //set date
-        $("#dateOfBirthInput").click();
-        $(".react-datepicker__month-select").selectOption(monthOfBirth);
-        $(".react-datepicker__year-select").selectOption(yearOfBirth);
-        $(".react-datepicker__day--0" + dayOfBirth).click();
-        //set subjects
-        $("#subjectsInput").val(subject1Prefix);
-        $(".subjects-auto-complete__menu-list").$(byText(subject1)).click();
-        $("#subjectsInput").val(subject2Prefix);
-        $(".subjects-auto-complete__menu-list").$(byText(subject2)).click();
-        $("#subjectsInput").val(subject3Prefix);
-        $(".subjects-auto-complete__menu-list").$(byText(subject3)).click();
-        //set hobbies
-        $("#hobbiesWrapper").$(byText(hobbie1)).click();
-        $("#hobbiesWrapper").$(byText(hobbie2)).click();
+            $("#uploadPicture").uploadFile(new File("src/test/resources/" + picture));
 
-        $("#uploadPicture").uploadFile(new File("src/test/resources/" + picture));
+            $("#currentAddress").val(currentAddress);
+            //set state and city
+            $("#state").click();
+            $("#stateCity-wrapper").$(byText(state)).click();
+            $("#city").click();
+            $("#stateCity-wrapper").$(byText(city)).click();
 
-        $("#currentAddress").val(currentAddress);
-        //set state and city
-        $("#state").click();
-        $("#stateCity-wrapper").$(byText(state)).click();
-        $("#city").click();
-        $("#stateCity-wrapper").$(byText(city)).click();
+            $("#submit").click();
+        });
 
-        $("#submit").click();
-
-        //asserts
-        $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
-        //$(".table-responsive").shouldHave(text(firstName + " " + lastName),
-                //text(email), text (gender));
-        //тоже самое, что и сверху
-        $x("//td[text()='Student Name']").parent().shouldHave(text(firstName + " " + lastName));
-        $x("//td[text()='Student Email']").parent().shouldHave(text(userEmail));
-        $x("//td[text()='Gender']").parent().shouldHave(text(gender));
-        $x("//td[text()='Mobile']").parent().shouldHave(text(userNumber));
-        $x("//td[text()='Date of Birth']").parent().shouldHave(text(dayOfBirth + " " + monthOfBirth + "," + yearOfBirth));
-        $x("//td[text()='Subjects']").parent().shouldHave(text(subject1 + ", " + subject2 + ", " + subject3));
-        $x("//td[text()='Hobbies']").parent().shouldHave(text(hobbie1 + ", " + hobbie2));
-        $x("//td[text()='Picture']").parent().shouldHave(text(picture));
-        $x("//td[text()='Address']").parent().shouldHave(text(currentAddress));
-        $x("//td[text()='State and City']").parent().shouldHave(text(state + " " + city));
+        step("Verify succesfull form submit", () -> {
+            $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
+            //$(".table-responsive").shouldHave(text(firstName + " " + lastName),
+                    //text(email), text (gender));
+            //тоже самое, что и сверху
+            $x("//td[text()='Student Name']").parent().shouldHave(text(firstName + " " + lastName));
+            $x("//td[text()='Student Email']").parent().shouldHave(text(userEmail));
+            $x("//td[text()='Gender']").parent().shouldHave(text(gender));
+            $x("//td[text()='Mobile']").parent().shouldHave(text(userNumber));
+            $x("//td[text()='Date of Birth']").parent().shouldHave(text(dayOfBirth + " " + monthOfBirth + "," + yearOfBirth));
+            $x("//td[text()='Subjects']").parent().shouldHave(text(subject1 + ", " + subject2 + ", " + subject3));
+            $x("//td[text()='Hobbies']").parent().shouldHave(text(hobbie1 + ", " + hobbie2));
+            $x("//td[text()='Picture']").parent().shouldHave(text(picture));
+            $x("//td[text()='Address']").parent().shouldHave(text(currentAddress));
+            $x("//td[text()='State and City']").parent().shouldHave(text(state + " " + city));
+        });
     }
 }
